8505e39e55a67ecfd483e8d6dd2ed022ebbdac71
mozack29 <73516573+mozack29@users.noreply.github.com>
2021-08-12 16:41:29 +0530

    Add files via upload

---
+++ imageconfig.js
@@ -0,0 +1,162 @@
+    function addtocart(){
+		console.log(finalObj);
+	}
+	
+	function changeImage(imageValue) {
+
+            configuratorobj.setConfiguration({
+                'sourceImage': {
+                    assetId: imageValue
+                }
+            });
+
+        }
+		
+	async function getMaterialConfigurator() {
+            const panelid = playerObj.configurator.appliedConfiguration["panelName"]
+            var assetInstance = await playerObj.getAssetInstance({
+                id: panelid,
+                plug: "Proxy",
+                property: "asset"
+            })
+            configuratorobj = await playerObj.getConfiguratorInstance({
+                from: api.scene.findNode({
+                    from: assetInstance,
+                    name: selectNodeName
+                }),
+                name: 'FrontFace',
+                plug: 'Material',
+                property: 'reference'
+            })
+        }
+		
+	function addToolToScene() {
+
+            api.tools.addTool({
+                    key: 'CanvasPrint',
+                    label: 'Select',
+                    active: true,
+                    enabled: true,
+                    handlers: {
+
+                        mousedown: e => {
+
+                            api.selectionSet.clear();
+
+                            const hits = e.hitNodes;
+                            if (!hits.length) return;
+
+                            var id = hits.length - 1;
+                            var selectMeshID;
+                            var selectCanvasName;
+                            var selectCanvasID;
+                            const hierarchy = [...hits[id].hierarchy];
+                            hierarchy.reverse();
+
+                            var canvas = hierarchy.find(el => el.type === "Null");
+                            var panel = hierarchy.find(el => el.type === "PolyMesh" && el.name === "FrontFace");
+
+                            if (panel) {
+                                api.selectionSet.add(panel.nodeId);
+                                selectNodeName = canvas.name;
+                                //console.log(selectNodeName);
+                                if (!finalObj[selectNodeName])
+                                    finalObj[selectNodeName] = {}; //creating object
+                                //console.log(finalObj);
+                                //node.setStyle({ outlineColor: '#00ff00', outlineThinkness: 5, color: '#ff0000', opacity: 0.5 });
+                                getMaterialConfigurator().then();
+                            }
+
+                        }, //end of mouse event
+
+
+                    }, //end of handelers
+                } //end fo add tool
+
+            );
+
+        } //end of function 
+
+	function movefourdirection(direction) {
+            var verticalPos = configuratorobj.appliedConfiguration["canvas_vertical_global"];
+            var horizontalPos = configuratorobj.appliedConfiguration["canvas_horizontal_global"];
+
+            if (direction == 'Right') {
+                horizontalPos = horizontalPos + 20;
+            }
+
+            if (direction == 'Left') {
+                horizontalPos = horizontalPos - 20;
+            }
+
+            if (direction == 'Down') {
+                verticalPos = verticalPos + 20;
+            }
+
+            if (direction == 'Up') {
+                verticalPos = verticalPos - 20;
+            }
+            finalObj[selectNodeName].verticalPos = verticalPos;
+            finalObj[selectNodeName].horizontalPos = horizontalPos;
+
+            configuratorobj.setConfiguration({
+                "canvas_vertical_global": verticalPos,
+                "canvas_horizontal_global": horizontalPos
+            })
+
+        }
+
+        function rotateimage() {
+
+            var rotate = configuratorobj.appliedConfiguration["canvas_rotation_global"];
+            rotate = rotate + 5;
+            finalObj[selectNodeName].rotate = rotate;
+            configuratorobj.setConfiguration({
+                "canvas_rotation_global": rotate
+            })
+
+        }
+
+        function blacknwhiteimage() {
+
+            var bw = configuratorobj.appliedConfiguration["blackwhite_global"];
+            //default false
+            if (bw == false) {
+                configuratorobj.setConfiguration({
+                    "blackwhite_global": true
+                })
+				finalObj[selectNodeName].blacknwhite = true;
+
+            } else {
+                configuratorobj.setConfiguration({
+                    "blackwhite_global": false
+                })
+				finalObj[selectNodeName].blacknwhite = false;
+
+            }
+        }
+
+
+        function zoominoutimage(scale) {
+
+            var height = configuratorobj.appliedConfiguration["canvas_height_global"];
+            var width = configuratorobj.appliedConfiguration["canvas_width_global"];
+
+            if (scale == 'Plus') {
+                height = height + .1;
+                width = width + .1;
+            }
+
+            if (scale == 'Minus') {
+                height = height - .1;
+                width = width - .1;
+            }
+            finalObj[selectNodeName].height = height;
+            finalObj[selectNodeName].width = width;
+
+            configuratorobj.setConfiguration({
+                "canvas_height_global": height,
+                "canvas_width_global": width
+            })
+
+        }
---
+++ uploadFiles.js
@@ -0,0 +1,80 @@
+async function uploadImage(filedata)
+{
+	//This method returns the fileID and AssetID
+	var jobID= await uploadFileToThreeKit(filedata);
+	var objAsset=await getAssetIDfromJob(jobID);
+while(objAsset?.job_status!= "stopped")	
+{
+	objAsset=await getAssetIDfromJob(jobID);
+	await sleep(3000);
+}
+finalObj[selectNodeName].fileID = objAsset.imagefileId;
+finalObj[selectNodeName].assetId = objAsset.imageassetId;
+	return objAsset;
+	
+}
+
+async function uploadFileToThreeKit(filedata) {
+
+    var myHeaders = new Headers(); //post 
+    var jobIdGlobal;
+    var formdata = new FormData();
+    formdata.append("orgId", orgID);
+    formdata.append("files", filedata);
+
+    var requestOptionsUpload = {
+        method: 'POST',
+        headers: myHeaders,
+        body: formdata,
+        redirect: 'follow'
+    };
+
+   
+
+    await fetch("https://preview.threekit.com/api/catalog/assets/upload?bearer_token=" + authToken, requestOptionsUpload)
+        .then(response => response.json())
+        .then((result) => {
+                //						
+                jobIdGlobal = result[0].jobId;
+                
+               
+				
+            } //end of then result
+        )
+
+    .catch(error => console.log('error', error));
+	
+return jobIdGlobal;
+ }
+//This function will return fileID and assetId.
+async function  getAssetIDfromJob(jobID)
+{
+	 var myHeadersAsset = new Headers(); //get
+	 var imageassetId;
+	 var imagefileId;
+	 var job_status;
+	 var requestOptionsAsset = {
+        method: 'GET',
+        headers: myHeadersAsset,
+        redirect: 'follow'
+    };
+	
+	await fetch("https://preview.threekit.com/api/catalog/jobs/" + jobID + "?bearer_token=" + authToken + "&orgId" + orgID, requestOptionsAsset)
+                        .then(responseAsset => responseAsset.json())
+                        .then((resultAsset) => {
+                                job_status = resultAsset.job.status;
+                                if (job_status === "stopped") {
+                                  								
+                                    imageassetId = resultAsset?.output?.texture[0]?.assetId;
+									imagefileId = resultAsset?.job?.parameters?.fileId;
+							
+                                }
+                            } //end then resultAsset
+                        ) //end then resultAsset
+                        .catch(error => console.log('error', error));
+						return {job_status,imageassetId, imagefileId};
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
